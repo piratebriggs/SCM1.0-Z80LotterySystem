d0412ab568e32f17112122497fa259aaf1694dbd
Antony Briggs <antony.briggs@gmail.com>
2024-08-12 13:58:48 +0100

    connected

--- Test/Test.asm
+++ Test/Test.asm
@@ -26,27 +26,73 @@ ColdStrt:   DI                  ;Disable interrupts
             CALL RC2014_SerialSIO2_IniSend
 
 Loop:
-            LD   DE,szStartup   ;="Devices:"
+            LD A,$08            ; flash
+            OUT (PIO_C),A		; Set port A 
+            CALL medium_delay
+            LD A,$00            ; all off
+            OUT (PIO_C),A		; Set port A 
+            CALL medium_delay
+
+            LD   DE,szStartup   ;
             CALL OutputZString  ;Output message at DE
 
+CH_Reset:
+            ld a, RESET_ALL
+            OUT (CH_CMD),A      ; send
+            CALL medium_delay
+
+
+CH_Host_Mode:
+            ld a, SET_USB_MODE
+            OUT (CH_CMD),A      ; send
+            ld a, 6
+            OUT (CH_DAT),A      ; send
+            CALL short_delay
+            IN A,(CH_DAT)       ; read
+            CALL PrintHexByte
+            cp CMD_RET_SUCCESS
+            jr z, CH_Disk_Conect
+            JP Loop
+
+
+CH_Disk_Conect:
+            ld a, DISK_CONNECT
+            OUT (CH_CMD),A      ; send
+            call CH_Chk_Success
+            jr z, CH_Disk_Mount
+            JP Loop
+
+CH_Disk_Mount:
+        	ld a, DISK_MOUNT
+            OUT (CH_CMD),A      ; send
+            call CH_Chk_Success
+            jr z, CH_Exists
+            JP Loop
+
+CH_Exists:
             LD A,$01           ; Get Ver
             OUT (CH_CMD),A      ; send
             IN A,(CH_DAT)       ; read
-            CALL StrWrHexByte
+            CALL PrintHexByte
 
             LD A,$06           ; CHK
             OUT (CH_CMD),A      ; send
             LD A,$03           ; data
             OUT (CH_CMD),A      ; send
             IN A,(CH_DAT)       ; read
-            CALL StrWrHexByte
+            CALL PrintHexByte
 
-            LD A,$02            ; all off
-            OUT (PIO_C),A		; Set port A 
-            LD BC,$FFFF         ; long-ish delay
-            CALL DELAY
 
             JP Loop
+
+
+CH_Chk_Success:
+            ld a, GET_STATUS
+            OUT (CH_CMD),A      ; send
+            IN A,(CH_DAT)       ; read
+            call PrintHexByte
+            cp USB_INT_SUCCESS
+            ret
 
 ; RC2014 serial SIO/2 write initialisation data 
 ;   On entry: C = Address of SIO control register
@@ -106,10 +152,10 @@ Next:       LD   A,(DE)         ;Get character from string
 Finished:   POP  AF
             RET
 
-; String: Write hex byte to string buffer
+; Write hex byte
 ;   On entry: A = Hex byte
 ;   On exit:  AF BC DE HL IX IY I AF' BC' DE' HL' preserved
-StrWrHexByte:
+PrintHexByte:
             PUSH AF
             RRA                 ; Shift top nibble to
             RRA                 ;  botom four bits..
@@ -135,13 +181,21 @@ Skip:       ADD  A,$30         ;Add ASCII '0'
             POP  AF
             RET
 
+long_delay:
+	ld bc, 65000
+	jr delay
+medium_delay:
+	ld bc, 45000
+	jr delay
+short_delay:
+	ld bc, 100
 ; INPUT: Loop count in BC
-DELAY:
+delay:
             NOP
             DEC BC
             LD A,B
             OR C
-            JR NZ,DELAY
+            JR NZ,delay
             RET
 
             HALT
@@ -152,7 +206,60 @@ kReturn:    .EQU 13             ;Return character (0x0D)
 
 kSIOTxRdy:  .EQU 2              ;Transmit data empty bit number
 
-szStartup:  .DB "Z80-Lottery!",kReturn,kLinefeed,0
+szStartup:  .DB "Lot!",kReturn,kLinefeed,0
+
+
+GET_IC_VER: .equ $01
+SET_BAUDRATE: .equ $02
+RESET_ALL: .equ $05
+CHECK_EXIST: .equ $06
+GET_FILE_SIZE: .equ $0C
+SET_USB_MODE: .equ $15
+GET_STATUS: .equ $22
+RD_USB_DATA0: .equ $27
+WR_USB_DATA: .equ $2C
+WR_REQ_DATA: .equ $2D
+WR_OFS_DATA: .equ $2E
+SET_FILE_NAME: .equ $2F
+DISK_CONNECT: .equ $30
+DISK_MOUNT: .equ $31
+FILE_OPEN: .equ $32
+FILE_ENUM_GO: .equ $33
+FILE_CREATE: .equ $34
+FILE_ERASE: .equ $35
+FILE_CLOSE: .equ $36
+DIR_INFO_READ: .equ $37
+DIR_INFO_SAVE: .equ $38
+BYTE_LOCATE: .equ $39
+BYTE_READ: .equ $3A
+BYTE_RD_GO: .equ $3B
+BYTE_WRITE: .equ $3C
+BYTE_WR_GO: .equ $3D
+DISK_CAPACITY: .equ $3E
+DISK_QUERY: .equ $3F
+DIR_CREATE: .equ $40
+
+
+; Statuses
+USB_INT_SUCCESS: .equ $14
+USB_INT_CONNECT: .equ $15
+USB_INT_DISCONNECT: .equ $16
+USB_INT_BUF_OVER: .equ $17
+USB_INT_USB_READY: .equ $18
+USB_INT_DISK_READ: .equ $1D
+USB_INT_DISK_WRITE: .equ $1E
+USB_INT_DISK_ERR: .equ $1F
+YES_OPEN_DIR: .equ $41
+ERR_MISS_FILE: .equ $42
+ERR_FOUND_NAME: .equ $43
+CMD_RET_SUCCESS: .equ $51
+ERR_DISK_DISCON: .equ $82
+ERR_LARGE_SECTOR: .equ $84
+ERR_TYPE_ERROR: .equ $92
+ERR_BPB_ERROR: .equ $A1
+ERR_DISK_FULL: .equ $B1
+ERR_FDT_OVER: .equ $B2
+ERR_FILE_CLOSE: .equ $B4
 
             .END
 
